e92ac70e06ab3dd8bb93de75d1a1fa7710f09f69
John Duncan <john@johnduncanphoto.com>
2017-09-29 12:02:31 +0100

    passed test five

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,19 @@
 def pet_shop_name(pet_shop_name)
  return pet_shop_name[:name]
  end
+
+def total_cash (pet_shop_name)
+  return pet_shop_name[:admin][:total_cash]
+end
+
+def add_or_remove_cash( pet_shop_name, amount)
+  pet_shop_name[:admin][:total_cash] += amount
+end
+
+def add_or_remove_cash( pet_shop_name, amount)
+  pet_shop_name[:admin][:total_cash] += amount
+end
+
+def pets_sold (pet_shop_name)
+  pet_shop_name[:admin][:pets_sold]
+end
